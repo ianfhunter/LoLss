1b2a14d044a28506162fba4d316064560a8d6bd2
Ian Hunter <hunterif@tcd.ie>
2013-08-25 14:02:05 +0100

    All areas have timers, but are on 5min times [blue buff times]

---
+++ secondscreen/secondscreen/static/js/speech.bak.js
@@ -0,0 +1,62 @@
+
+
+$("svg").click(function(){
+	start = new Date();
+
+	var w = 100,
+	    h = 100,
+	    x = d3.scale.ordinal().domain(d3.range(3)).rangePoints([0, w], 2);
+
+	var fields = [
+	  {name: "seconds", value: 0, size: 60}
+	];
+
+	var svg = d3.selectAll("svg")
+	    .attr("width", w)
+	    .attr("height", h)
+	  .append("svg:g")
+	    .attr("transform", "translate(-10,15)");
+
+	setInterval(function() {
+	  var now = new Date();
+
+	  fields[0].previous = fields[0].value; 
+	  fields[0].value = now.getSeconds();
+
+	  var path = svg.selectAll("path")
+	      .data(fields.filter(function(d) { return d.value; }), function(d) { return d.name; });
+
+	  path.enter().append("svg:path")
+	      .attr("transform", function(d, i) { return "translate(" + x(i) + ",0)"; })
+	    .transition()
+	      .ease("elastic")
+	      .duration(750)
+	      .attrTween("d", arcTween);
+
+	  path.transition()
+	      .ease("elastic")
+	      .duration(750)
+	      .attrTween("d", arcTween);
+
+	  path.exit().transition()
+	      .ease("bounce")
+	      .duration(750)
+	      .attrTween("d", arcTween)
+	      .remove();
+
+	}, 1000);
+
+
+})
+function arcTween(b) {
+	var arc = d3.svg.arc()
+	    .innerRadius(0)
+	    .outerRadius(15)
+	    .startAngle(0)
+	    .endAngle(function(d) { return (d.value / d.size) * 2 * Math.PI; });
+
+  var i = d3.interpolate({value: b.previous}, b);
+  return function(t) {
+    return arc(i(t));
+  };
+}
---
+++ secondscreen/secondscreen/views.py
@@ -0,0 +1,13 @@
+
+from django.template import RequestContext, loader
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    template = loader.get_template('index.html')
+
+    context = RequestContext(request, {
+    #"match":match_settings,
+    })
+
+    return HttpResponse(template.render(context))
