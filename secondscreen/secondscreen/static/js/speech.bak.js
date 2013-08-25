e4b60e5ce3dae340da197481a60ca8a55b306d7a
Ian Hunter <hunterif@tcd.ie>
2013-08-25 15:35:25 +0100

    tickers now work errorless

--- secondscreen/secondscreen/static/js/speech.bak.js
+++ secondscreen/secondscreen/static/js/speech.bak.js
@@ -1,62 +1,77 @@
+function timeDiff(a,b){
+	return ((b.getMinutes() - a.getMinutes())*60) + b.getSeconds() - a.getSeconds()
+}
 
+function Timer(countdown,obj){
+	var that = this;
+	this.obj = obj;
+	this.countdown = countdown;
+	this.start = new Date();
+	this.intervalId = 0;
+	this.now = new Date();
+
+	this.activate = new function(){
+
+		var w = 100,
+		    h = 100,
+		    x = d3.scale.ordinal().domain(d3.range(3)).rangePoints([0, w], 2);
+		var fields = [
+		  {name: "seconds", value: 0, size: countdown}
+		];
+		var svg = d3.select(obj)
+		    .attr("width", w)
+		    .attr("height", h)
+		.append("svg:g")
+		    .attr("transform", "translate(-10,15)");
+
+		intervalId = setInterval(function() {
+		  now = new Date();
+
+		  fields[0].previous = fields[0].value; 
+		  fields[0].value = timeDiff(start,now);
+
+		  var path = svg.selectAll("path")
+		      .data(fields.filter(function(d) { return d.value; }), function(d) { return d.name; });
+
+		  path.enter().append("svg:path")
+		      .attr("transform", function(d, i) { return "translate(" + x(i) + ",0)"; })
+		    .transition()
+		      .ease("elastic")
+		      .duration(750)
+		      .attrTween("d", arcTween);
+
+		  path.transition()
+		      .ease("elastic")
+		      .duration(750)
+		      .attrTween("d", arcTween);
+
+		}, 1000);
+	}
+	this.arcTween = function(b){
+		var arc = d3.svg.arc()
+		    .innerRadius(0)
+		    .outerRadius(15)
+		    .startAngle(0)
+		    .endAngle(function(d) { 
+		    	if(d.value > countdown){
+		    		clearInterval(intervalId);
+		    		console.log(this)
+		    		$("g").remove();
+		    		return 2 * Math.PI
+		    	}
+				else if(d.value > 0){
+			    	return ((d.value/d.size) * 2 * Math.PI);
+				}else{
+					return 0;
+				}
+			})
+	  	var i = d3.interpolate({value: b.previous}, b);
+	  	return function(t) {
+	    	return arc(i(t));
+	  	};	
+	}
+}
 
 $("svg").click(function(){
-	start = new Date();
-
-	var w = 100,
-	    h = 100,
-	    x = d3.scale.ordinal().domain(d3.range(3)).rangePoints([0, w], 2);
-
-	var fields = [
-	  {name: "seconds", value: 0, size: 60}
-	];
-
-	var svg = d3.selectAll("svg")
-	    .attr("width", w)
-	    .attr("height", h)
-	  .append("svg:g")
-	    .attr("transform", "translate(-10,15)");
-
-	setInterval(function() {
-	  var now = new Date();
-
-	  fields[0].previous = fields[0].value; 
-	  fields[0].value = now.getSeconds();
-
-	  var path = svg.selectAll("path")
-	      .data(fields.filter(function(d) { return d.value; }), function(d) { return d.name; });
-
-	  path.enter().append("svg:path")
-	      .attr("transform", function(d, i) { return "translate(" + x(i) + ",0)"; })
-	    .transition()
-	      .ease("elastic")
-	      .duration(750)
-	      .attrTween("d", arcTween);
-
-	  path.transition()
-	      .ease("elastic")
-	      .duration(750)
-	      .attrTween("d", arcTween);
-
-	  path.exit().transition()
-	      .ease("bounce")
-	      .duration(750)
-	      .attrTween("d", arcTween)
-	      .remove();
-
-	}, 1000);
-
-
+	time = new Timer(10,this);
 })
-function arcTween(b) {
-	var arc = d3.svg.arc()
-	    .innerRadius(0)
-	    .outerRadius(15)
-	    .startAngle(0)
-	    .endAngle(function(d) { return (d.value / d.size) * 2 * Math.PI; });
-
-  var i = d3.interpolate({value: b.previous}, b);
-  return function(t) {
-    return arc(i(t));
-  };
-}
--- secondscreen/secondscreen/static/js/speech.js
+++ secondscreen/secondscreen/static/js/speech.js
@@ -1,75 +1,78 @@
+function timeDiff(a,b){
+	return ((b.getMinutes() - a.getMinutes())*60) + b.getSeconds() - a.getSeconds()
+}
 
+function Timer(countdown,obj){
+	var that = this;
+	this.obj = obj;
+	this.countdown = countdown;
+	this.start = new Date();
+	this.intervalId = 0;
+	this.now = new Date();
+
+	this.arcTween = function(b){
+		var arc = d3.svg.arc()
+		    .innerRadius(0)
+		    .outerRadius(15)
+		    .startAngle(0)
+		    .endAngle(function(d) { 
+		    	if(d.value > countdown){
+		    		clearInterval(that.intervalId);
+		    		$(that.obj).find("g").remove();
+		    		return 2 * Math.PI
+		    	}
+				else if(d.value > 0){
+			    	return ((d.value/d.size) * 2 * Math.PI);
+				}else{
+					return 0;
+				}
+			})
+	  	var i = d3.interpolate({value: b.previous}, b);
+	  	return function(t) {
+	    	return arc(i(t));
+	  	};	
+	}
+
+	this.activate = new function(){
+		var w = 100,
+		    h = 100,
+		    x = d3.scale.ordinal().domain(d3.range(3)).rangePoints([0, w], 2);
+		var fields = [
+		  {name: "seconds", value: 0, size: that.countdown}
+		];
+		var svg = d3.select(obj)
+		    .attr("width", w)
+		    .attr("height", h)
+		.append("svg:g")
+		    .attr("transform", "translate(-10,15)");
+
+		that.intervalId = setInterval(function() {
+		  that.now = new Date();
+
+
+		  fields[0].previous = fields[0].value; 
+		  fields[0].value = timeDiff(that.start,that.now);
+
+
+		  var path = svg.selectAll("path")
+		      .data(fields.filter(function(d) { return d.value; }), function(d) { return d.name; });
+
+		  path.enter().append("svg:path")
+		      .attr("transform", function(d, i) { return "translate(" + x(i) + ",0)"; })
+		    .transition()
+		      .ease("elastic")
+		      .duration(750)
+		      .attrTween("d", that.arcTween);
+
+		  path.transition()
+		      .ease("elastic")
+		      .duration(750)
+		      .attrTween("d", that.arcTween);
+
+		}, 1000);
+	}
+}
 
 $("svg").click(function(){
-	start = new Date();
-	timer = 10;
-
-	var w = 100,
-	    h = 100,
-	    x = d3.scale.ordinal().domain(d3.range(3)).rangePoints([0, w], 2);
-
-	var fields = [
-	  {name: "seconds", value: 0, size: timer}
-	];
-
-	var svg = d3.selectAll("svg")
-	    .attr("width", w)
-	    .attr("height", h)
-	  .append("svg:g")
-	    .attr("transform", "translate(-10,15)");
-
-	setId = setInterval(function() {
-	  var now = new Date();
-
-	  fields[0].previous = fields[0].value; 
-	  fields[0].value = ((now.getMinutes() - start.getMinutes())*60) + now.getSeconds() - start.getSeconds();
-
-	  var path = svg.selectAll("path")
-	      .data(fields.filter(function(d) { return d.value; }), function(d) { return d.name; });
-
-	  path.enter().append("svg:path")
-	      .attr("transform", function(d, i) { return "translate(" + x(i) + ",0)"; })
-	    .transition()
-	      .ease("elastic")
-	      .duration(750)
-	      .attrTween("d", arcTween);
-
-	  path.transition()
-	      .ease("elastic")
-	      .duration(750)
-	      .attrTween("d", arcTween);
-
-	  path.exit().transition()
-	      .ease("bounce")
-	      .duration(750)
-	      .attrTween("d", arcTween)
-	      .remove();
-
-	}, 1000);
-
-
+	time = new Timer(10,this);
 })
-
-var a;
-function arcTween(b) {
-	var arc = d3.svg.arc()
-	    .innerRadius(0)
-	    .outerRadius(15)
-	    .startAngle(0)
-	    .endAngle(function(d) { 
-	    	if(d.value > timer){
-	    		clearInterval(setId);
-	    		$("g").remove();
-	    	}
-			else if(d.value > 0){
-		    	return ((d.value/d.size) * 2 * Math.PI);
-			}else{
-				return 0;
-			}
-		})
-
-  	var i = d3.interpolate({value: b.previous}, b);
-  	return function(t) {
-    	return arc(i(t));
-  	};
-}
